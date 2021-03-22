3128a9b2d73a789684fc1f235c0a228ea813ad80
Jaewon Han <jaewonhan20@gmail.com>
2021-03-22 09:04:54 -0500

    Functions to edit the contents in the HTML

--- js/procedure.js
+++ js/procedure.js
@@ -2,3 +2,21 @@ $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
   
+
+  var procedure1;
+document.getElementById("newProcedure").onclick=editProcedure;
+
+
+function editProcedure(){
+  procedure1 = prompt("Edit the procedure: ");
+  if(procedure===null){
+   console.log(document.getElementById("procedure").innerHTML);
+  }
+  else{
+  updateProcedure();
+  }
+}
+
+function updateProcedure() {
+  document.getElementById("procedure").innerHTML = procedure1;
+}
--- js/symptom.js
+++ js/symptom.js
@@ -2,3 +2,22 @@ $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
   
+
+var symptom1;
+document.getElementById("newSymptom").onclick=editSymptom;
+
+
+function editSymptom(){
+  symptom1 = prompt("Edit the symptom: ");
+  if(symptom1===null){
+   console.log(document.getElementById("symptom").innerHTML);
+  }
+  else{
+  updateSymptom();
+  }
+}
+
+function updateSymptom() {
+  document.getElementById("symptom").innerHTML = symptom1;
+}
+
