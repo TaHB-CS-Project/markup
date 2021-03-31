7eece3a94638e307214402c6be2c951d5309c8bc
Jaewon Han <jaewonhan20@gmail.com>
2021-03-31 02:17:40 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $("#myform").validate({
         rules: {
-            password:{
-                required:true
+            "email":{
+                "required":true,
+                "email":true
             },
-            email:{
-                required:true,
-                email:true
+            "password":{
+                "required":true
             }
         },
         messages:{
@@ -23,26 +23,37 @@ $(document).ready(function(){
     var data=$("#login_form").serialize();
     $.ajax({
         type:'POST',
-        url:'call.go',
-        data: data,
+        url:'call.go'+'?rand=' +newDate().getTime(),
+        async:true,
+        dataType:"json",
+        data:{
+            email: $(`#email`).val(),
+            password: $(`#password`).val()
+        },
         beforeSend: function(){
             $("#info").fadeOut();
             $("#login").html("Please wait...");
         },
-        success: function(resp){
-            if(resp=="ok"){
-                $("#login").html("<img src='img/ajax-loader.gif' width='15'/> &nbsp; Login");
-                setTimeout('window.location.href= "dashboard.html";', 4000); //check
+        success: function(jsonData){
+            if(jsonData.hasErrors){
+                displayErrors(jasonData.errors);
             }
             else{
-                $("#info").fadeIn(1000, function(){
-                    $("#info").html("<div class='alaert alert-danger'>"+resp+"</div>");
-                    $("#login").html()
-                })
+               alert(jsonData.confirm);
+               //add code here
+               displayLogin();
             }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            $('#error').html(XMLHttpRequest.responseText).removeClass('hide').fadeIn('slow');
+
         }
-
-    })
+    });
 }
-
+function displayErrors(errors) {
+	str_errors = '<p><strong>' + (errors.length > 1 ? more_errors : one_error) + '</strong></p><ol>';
+	for (var error in errors) 
+		if (error != 'indexOf') str_errors += '<li>' + errors[error] + '</li>';
+	$('#error').html(str_errors + '</ol>').removeClass('hide').fadeIn('slow');
+}
 })
