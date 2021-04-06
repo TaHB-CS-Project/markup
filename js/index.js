73595c9c48924a4959ad327903fd8502cdd1d621
Jaewon Han <jaewonhan20@gmail.com>
2021-04-06 11:11:55 -0500

    !!!!

--- js/index.js
+++ js/index.js
@@ -10,45 +10,24 @@ $(document).ready(function() {
                     url:'http://127.0.0.1:8090/user1.go',    
                     type:"POST",   
                     dataType:"JSON", 
-<<<<<<< HEAD
-                    data: JSON.stringify({Username: email, Password_hash: password}),
-                    
-                  
-                    success:function(response){
-                        console.log(Response);
-                        login = JSON.stringify(response)
-                        if(login == '{"Correctcredentials":true}'){
-                            Swal.fire({
-                                icon: 'success',
-                                title: 'Welcome!',
-                              
-                                
-                            });
-                         
-                            window.location = "dashboard.html";                
-                        }
-                        else if(login == '{"Incorrectcredentials":false}'){
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Invalid Username or Password',
-                                text: 'Please try again'
-                            });
-                            return;
-=======
                     data: JSON.stringify({"Username": email, "Password_hash": password}),
                     success:function(response){  
+                        console.log(response);
                         login = JSON.stringify(response)
                         if(login == '{"Correctcredentials":true}'){
-                            alert("Login Successful")
-                            window.location = "dashboard.html";                    
+                            if(login == '{"Correctcredentials":true}'){
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Welcome!',
+                                });
+                             
+                                window.location = "dashboard.html";                     
                         }
                         else if(login == '{"Incorrectcredentials":false}'){
                             login_result.innerHTML = "<p>Your username or password is incorrect\nPlease try again</p>"
                         }
-                        else{
-                            alert("Unexpected Error")
->>>>>>> 0c96915f3ea7f70b121c3f7427764415c3540577
-                        }
+                     
+            }
                         else{
                             alert("Unexpected Error")
                         }
